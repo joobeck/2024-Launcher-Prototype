774091c7efb94c69fa9a818339a5117eef04d66d
Team 60 <firstteam60robotics@gmail.com>
2023-12-04 19:45:34 -0700

    initial upload of swerve code

---
+++ src/main/java/frc/robot/SwerveDrive.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+// Add correct length and width
+// convert angle here to angle elsewhere
+// check SparkMax CAN numbers
+
+/** Add your docs here. */
+public class SwerveDrive {
+    private WheelDrive backRight;
+    private WheelDrive backLeft;
+    private WheelDrive frontRight;
+    private WheelDrive frontLeft;
+
+    public final double length = 0;
+    public final double width = 0;
+
+    // SwerveDrive constructor
+    public SwerveDrive (WheelDrive backRight, WheelDrive backLeft, WheelDrive frontRight, WheelDrive frontLeft){
+        this.backRight = backRight;
+        this.backLeft = backLeft;
+        this.frontRight = frontRight;
+        this.frontLeft = frontLeft;
+    }
+    // drive method
+    public void drive (double x1, double y1, double x2) {
+        double rotation = Math.sqrt((length * length) + (width * width));
+        y1 = y1* -1;
+
+        double a = x1 - x2 * (length / rotation);
+        double b = x1 + x2 * (length / rotation);
+        double c = y1 - x2 * (width / rotation);
+        double d = y1 + x2 * (width / rotation);
+
+        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
+        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
+        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
+        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+
+        double backRightAngle = 180 * (Math.atan2(a, d) / Math.PI);
+        double backLeftAngle = 180 * (Math.atan2(a, c) / Math.PI);
+        double frontRightAngle = 180 * (Math.atan2(b, d) / Math.PI);
+        double frontLeftAngle = 180 * (Math.atan2(b, c) / Math.PI);
+        //
+        backRight.drive(backRightSpeed, backRightAngle);
+        backLeft.drive(backLeftSpeed, backLeftAngle);
+        frontRight.drive(frontRightSpeed, frontRightAngle);
+        frontLeft.drive(frontLeftSpeed, frontLeftAngle);
+    }
+    
+}
---
+++ src/main/java/frc/robot/WheelDrive.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+// Angle multiplyer 
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
+
+/** Add your docs here. */
+public class WheelDrive {
+    private CANSparkMax angleMotor;
+    private CANSparkMax speedMotor;
+    private SparkMaxPIDController m_pidController;
+
+    private CANCoder absoluteEncoder;
+
+    // PID coefficients
+    public double kP = 0.1;
+    public double kI = 1;
+    public double kD = 1;
+    public double kIz = 0;
+    public double kFF = 0;
+    public double kMaxOutput = 1;
+    public double kMinOutput = -1;
+    public double PositionConversionFactor = 1;
+   
+    
+    //consider changing variable names
+    public WheelDrive (int angleMotor, int speedMotor) {
+        
+        this.angleMotor = new CANSparkMax(angleMotor, MotorType.kBrushless);
+        this.speedMotor = new CANSparkMax(speedMotor, MotorType.kBrushless);
+       
+        m_pidController = this.angleMotor.getPIDController();
+
+        //this.absoluteEncoder = new 
+
+        // set PID coefficients
+        m_pidController.setP(kP);
+        m_pidController.setI(kI);
+        m_pidController.setD(kD);
+        m_pidController.setIZone(kIz);
+        m_pidController.setFF(kFF);
+        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+        m_pidController.setPositionPIDWrappingMaxInput(180);
+        m_pidController.setPositionPIDWrappingMinInput(-180);
+       
+        //m_pidController.setFeedbackDevice(null);
+        //m_pidController.set
+
+        /**this.pidController.setPID(angleMotor, speedMotor, angleEncoder);
+        this.pidController.atSetpoint();
+        this.pidController.setSetpoint(angle);
+        */
+                
+        //this.angleMotor.getEncoder();        
+
+    }
+    public void drive (double speed, double angle){
+        // Sets the speed on the speed motor.
+        speedMotor.set(speed);
+        // 
+        angle = angle * 23;
+        m_pidController.setReference(angle, CANSparkMax.ControlType.kPosition);
+
+
+    }
+
+    public void zeroEncoders(){
+
+    }    
+
+}

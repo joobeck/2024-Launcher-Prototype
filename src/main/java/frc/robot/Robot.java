b5ccfc632d0a98d9eb9ab30172e698318eb7b81c
joobeck <joobeck@hotmail.com>
2024-02-01 15:50:29 -0800

    Removed extraneous code to give a very basic robot to start with.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,7 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
 // set spark max encoder from absolute encoder
 // add motor CAN numbers
 // add motor watch dogs
@@ -13,23 +12,14 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.SPI;
-
-import com.kauailabs.navx.frc.AHRS;
-import com.kauailabs.navx.frc.Quaternion;
 
 import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj.
-//import com.ctre.phoenix.sensors.
-/*import com.ctre.phoenix.sensors.CANCoder;
-import com.ctre.phoenix.sensors.WPI_CANCoder;
-import com.ctre.phoenix.sensors.CANCoderConfiguration;
-import com.ctre.phoenix.sensors.CANCoderStatusFrame;*/
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -38,26 +28,12 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  //private final CANCoder AbsoluteEncoder = new CANCoder(0);
-  //private WPI_CANCoder CANCoder = new 
-
-  AHRS gyro = new AHRS(SPI.Port.kMXP);
-
-
   // Joystick
-  private XboxController controller = new XboxController(0);
-
-  // Swerve drive
-  private WheelDrive backLeft = new WheelDrive(2, 1,0);
-  private WheelDrive backRight = new WheelDrive(4, 3,1);
-  private WheelDrive frontLeft = new WheelDrive(8, 7,3);
-  private WheelDrive frontRight = new WheelDrive(6, 5,2);
-  private SwerveDrive swerveDrive = new SwerveDrive(backRight, backLeft, frontRight, frontLeft);
-
+  // private XboxController controller = new XboxController(0);
 
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -65,69 +41,30 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    //AbsoluteEncoder.configSensorDirection(false);
-    //AbsoluteEncoder.configAbsoluteSensorRange
-    //AbsoluteEncoder.getAbsolutePosition();
-    
-    backRight.zeroEncoders(0.3389 * 360);
-    backLeft.zeroEncoders(0.7695 * 360);
-    frontRight.zeroEncoders(0.0614 * 360);
-    frontLeft.zeroEncoders(0.8470 * 360);
-
-    frontRight.invertAngleMotor(false);
-    frontLeft.invertAngleMotor(false);
-    backRight.invertAngleMotor(false);
-    backLeft.invertAngleMotor(false);
-
-    frontRight.invertDriveMotor(false);
-    frontLeft.invertDriveMotor(true);
-    backRight.invertDriveMotor(false);
-    backLeft.invertDriveMotor(true);
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *s
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics that you want ran during disabled, autonomous, teleoperated
+   * and test. s
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Relative Encoders
-    SmartDashboard.putNumber("backLeft relative encoder", backLeft.returnRelative());
-    SmartDashboard.putNumber("backRight relative encoder", backRight.returnRelative());
-    SmartDashboard.putNumber("frontRight relative encoder", frontRight.returnRelative());
-    SmartDashboard.putNumber("frontLeft relative encoder", frontLeft.returnRelative());
-    // Absolute Encoders
-    SmartDashboard.putNumber("backLeft AbsoluteEncoder", backLeft.returnabsolute());
-    SmartDashboard.putNumber("backRight AbsoluteEncoder", backRight.returnabsolute());
-    SmartDashboard.putNumber("frontLeft AbsoluteEncoder", frontLeft.returnabsolute());
-    SmartDashboard.putNumber("frontRight AbsoluteEncoder", frontRight.returnabsolute());
-
-    // Gyro angle
-    SmartDashboard.putNumber("Yaw", gyro.getYaw());
-
-    //Drive method
-    SmartDashboard.putNumber("desiredYaw", swerveDrive.returnDesiredYaw());
-    SmartDashboard.putNumber("turning",swerveDrive.returnTurning() );
-
-
-    //
-    SmartDashboard.putNumber("desiredYaw", swerveDrive.returnDesiredYaw());
-    SmartDashboard.putNumber("currentAngle",backRight.returncurrentAngle());
-
   }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -153,53 +90,40 @@ public class Robot extends TimedRobot {
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-    swerveDrive.setDesiredYaw(gyro.getYaw());
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    //swerveDrive.drive(controller.getLeftX(), controller.getLeftY(), controller.getRightX(), gyro.getYaw());
-    if (controller.getYButton()){
-      //North
-      swerveDrive.setDesiredYaw(0);
-    } else if(controller.getAButton()){
-      //South
-      swerveDrive.setDesiredYaw(180);
-    }else if(controller.getBButton()){
-      //East
-      swerveDrive.setDesiredYaw(90);;
-    } else if(controller.getXButton()){
-      //West
-      swerveDrive.setDesiredYaw(-90);;
-    } else {
-      swerveDrive.drive(controller.getLeftX(), controller.getLeftY(), controller.getRightX(), gyro.getYaw());
-    }
-
-
   }
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
